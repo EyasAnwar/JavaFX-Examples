81b3fa043d25a99c42dcbb8def8bbac412a91895
Eyas Anwar <eyasanwar@gmail.com>
2022-02-28 21:33:42 +0200

    init commit

---
+++ src/javafx/chapter14/BindingDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafx.chapter14;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class BindingDemo {
+
+    public static void main(String[] args) {
+        DoubleProperty d1 = new SimpleDoubleProperty(1);
+        DoubleProperty d2 = new SimpleDoubleProperty(2);
+        d1.bind(d2); // Bind d1 with d2
+        System.out.println("d1 is " + d1.getValue()
+                + " and d2 is " + d2.getValue());
+        d2.setValue(70.2);
+        System.out.println("d1 is " + d1.getValue()
+                + " and d2 is " + d2.getValue());
+    }
+}
