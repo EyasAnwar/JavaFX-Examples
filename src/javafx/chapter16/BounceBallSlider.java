33e4bd2bd4882b766c58342a112ded7faba0af1b
Eyas Anwar <eyasanwar@gmail.com>
2022-03-25 17:38:34 +0200

    add chapter 16

---
+++ src/javafx/chapter16/BounceBallSlider.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafx.chapter16;
+
+import javafx.application.Application;
+import javafx.chapter15.BallPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.BorderPane;
+
+public class BounceBallSlider extends Application {
+
+    @Override // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        BallPane ballPane = new BallPane();
+        Slider slSpeed = new Slider();
+        slSpeed.setMax(20);
+        ballPane.rateProperty().bind(slSpeed.valueProperty());
+
+        BorderPane pane = new BorderPane();
+        pane.setCenter(ballPane);
+        pane.setBottom(slSpeed);
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(pane, 250, 250);
+        primaryStage.setTitle("BounceBallSlider"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited JavaFX support.
+     * Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/javafx/chapter16/TextFieldDemo.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafx.chapter16;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class TextFieldDemo extends RadioButtonDemo {
+
+    @Override // Override the getPane() method in the super class
+    protected BorderPane getPane() {
+        BorderPane pane = super.getPane();
+
+        BorderPane paneForTextField = new BorderPane();
+        paneForTextField.setPadding(new Insets(5, 5, 5, 5));
+        paneForTextField.setStyle("-fx-border-color: green");
+        paneForTextField.setLeft(new Label("Enter a new message: "));
+
+        TextField tf = new TextField();
+        tf.setAlignment(Pos.BOTTOM_RIGHT);
+        paneForTextField.setCenter(tf);
+        pane.setTop(paneForTextField);
+
+        tf.setOnAction(e -> text.setText(tf.getText()));
+
+        return pane;
+    }
+
+    /**
+     * The main method is only needed for the IDE with limited JavaFX support.
+     * Not needed for running from the command line.
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
